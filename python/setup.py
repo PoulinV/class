0ae7c60f8a46e67dba8f874e8ec4b5d9a55396e1
Vivian Poulin <poulin@lapth.cnrs.fr>
2019-11-10 16:09:05 +0100

    small modif, moslty in gitignore

--- python/setup.py
+++ python/setup.py
@@ -9,13 +9,13 @@ import os.path as osp
 
 # Recover the gcc compiler
 GCCPATH_STRING = sbp.Popen(
-    ['gcc', '-print-libgcc-file-name'],
+    ['gcc-7', '-print-libgcc-file-name'],
     stdout=sbp.PIPE).communicate()[0]
 GCCPATH = osp.normpath(osp.dirname(GCCPATH_STRING)).decode()
 
 liblist = ["class"]
 MVEC_STRING = sbp.Popen(
-    ['gcc', '-lmvec'],
+    ['gcc-7', '-lmvec'],
     stderr=sbp.PIPE).communicate()[1]
 if b"mvec" not in MVEC_STRING:
     liblist += ["mvec","m"]
